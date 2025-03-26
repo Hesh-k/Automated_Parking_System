8b6f155575f17b642512283a3e914c67fa3d329d
AvishkaMalith <121160065+Avishka2288123095@users.noreply.github.com>
2025-03-26 10:35:48 +0530

    Adding slots and displaying parking map are done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 // src/App.jsx
 import React from 'react';
 import Home from './pages/Home';
+import './index.css';
+import AdminPanel from './pages/AdminPanel.jsx';
 
 const App = () => {
   return (
     <div className="app">
-      <Home />
+      <AdminPanel />
     </div>
   );
 };
